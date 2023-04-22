ce601db11d7da0b87e1765d5a16b112b883bcaf9
KarolStefaniakk <dsw48791@student.dsw.edu.pl>
2023-04-22 14:17:28 +0200

    Dodano skrypt help.sh

---
+++ help.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ "$1" = "--help" ]; then
+    echo "Dostępne opcje:"
+    echo "--date: Wyświetla dzisiejsza date"
+    echo "--logs: Tworzy 100 plikow logx.txt"
+    echo "--logsx [LICZBA]: Tworzy N plikow logx.txt"
+    echo "--help: Wyświetla ten komunikat pomocy."
