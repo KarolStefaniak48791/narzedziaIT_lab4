a1bed9877e15da80ee0879f6bcd2e897bd040d95
KarolStefaniakk <dsw48791@student.dsw.edu.pl>
2023-04-22 14:55:09 +0200

    Dodano nowe flagi do help

--- help.sh
+++ help.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     echo "Dostępne opcje:"
-    echo "--date: Wyświetla dzisiejsza date"
-    echo "--logs: Tworzy 100 plikow logx.txt"
-    echo "--logsx [LICZBA]: Tworzy N plikow logx.txt"
-    echo "--help: Wyświetla ten komunikat pomocy."
+    echo "--date, -d: Wyświetla dzisiejsza date"
+    echo "--logs, -l: Tworzy 100 plikow logx.txt"
+    echo "--logsx [LICZBA], x [LICZBA]: Tworzy N plikow logx.txt"
+    echo "--help, -h: Wyświetla ten komunikat pomocy."
