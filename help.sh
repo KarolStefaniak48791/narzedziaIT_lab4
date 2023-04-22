dc76a444408b8e75d19bcb23447ece96ad412776
KarolStefaniakk <dsw48791@student.dsw.edu.pl>
2023-04-22 15:06:18 +0200

    Zmodyfikowano help.sh

--- help.sh
+++ help.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     echo "Dostępne opcje:"
-    echo "--date: Wyświetla dzisiejsza date"
-    echo "--logs: Tworzy 100 plikow logx.txt"
-    echo "--logsx [LICZBA]: Tworzy N plikow logx.txt"
-    echo "--help: Wyświetla ten komunikat pomocy."
+    echo "--date, -d: Wyświetla dzisiejsza date"
+    echo "--logs, -l: Tworzy 100 plikow logx.txt"
+    echo "--logsx [LICZBA], x [LICZBA]: Tworzy N plikow logx.txt"
+    echo "--help, -h: Wyświetla ten komunikat pomocy."
+    echo "--init, -i: Klonuje całe repozytorium do katalogu w którym został uruchomiony oraz ustawia ścieżkę w zmiennej środowiskowej PATH."
+    echo "--error [LICZBA], -e [LICZBA]: tworzy errorx/errorx.txt."
