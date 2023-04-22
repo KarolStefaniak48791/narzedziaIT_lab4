098862f6dbd9113af798c7fdc16805f89b884710
KarolStefaniakk <dsw48791@student.dsw.edu.pl>
2023-04-22 14:15:27 +0200

    Dodano skrypt logsx.sh

---
+++ logsx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$1" = "--logsx" ]; then
+    if [ -n "$2" ]; then
+        count="$2"
+    else
+        count="30"
+    fi
+    for i in $(seq 1 "$count"); do
+        echo "Nazwa pliku: log$i.txt" > log$i.txt
+        echo "Nazwa skryptu: $0" >> log$i.txt
+        echo "Data: $(date)" >> log$i.txt
+    done
+fi
+
